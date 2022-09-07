21837d1149704f37204cfce01e0994619b6708f9
Oyinkansola Shoroye <oyinkansolashoroye@gmail.com>
2022-09-07 08:04:54 +0100

    init

---
+++ js/quiz.js
@@ -0,0 +1,81 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Look at this series: 2, 1, (1/2), (1/4)... What number should come next?",
+    answer: "1/8",
+    options: [
+      "1/3",
+      "1/8",
+      "2/8",
+      "1/16"
+    ] 
+  },
+    {
+    numb: 2,
+    question: "Which of the following can be arranged into a 5 letter english word?",
+    answer: "APSEH",
+    options: [
+      "FGOAT",
+      "APSEH",
+      "RYIPO",
+      "DAEPW"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which one of the four is least like the other three?",
+    answer: "bicycle",
+    options: [
+      "car",
+      "bus",
+      "bicycle",
+      "van"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Bob is 6 years old and his sister is half of his age. When Bob is 30, what will be the age of his sister",
+    answer: "27",
+    options: [
+      "20",
+      "15",
+      "25",
+      "27"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What number is the best match 8:4 as 10:?",
+    answer: "5",
+    options: [
+      "20",
+      "15",
+      "5",
+      "10"
+    ]
+  },
+    {
+    numb: 6,
+    question: "Look at this series: 7, 10, 8, 11, 9, 12... What number should come next?",
+    answer: "10",
+    options: [
+      "7",
+      "10",
+      "12",
+      "13"
+    ]
+  },
+    {
+    numb: 7,
+    question: "Janet is older than Oyin. Pelumi is older than Janet. Oyin is older than Pelumi. If the first two statements are true, the third statement is?",
+    answer: "false",
+    options: [
+      "true",
+      "false",
+      "uncertain",
+      "I don’t know"
+    ]
+  },
+ 
+];
